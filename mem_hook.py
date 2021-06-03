e4abca36d348ba81accf4f86e9034161c4d1e077
kev-damian <maheu@outlook.com>
2021-06-04 01:35:33 +0900

    memory profiling basic implement

---
+++ mem_hook.py
@@ -0,0 +1,29 @@
+from apex import amp
+from torch.utils.checkpoint import checkpoint_sequential
+import torch
+
+
+def _get_gpu_mem(synchronize=True, empty_cache=True):
+    return torch.cuda.memory_allocated(), torch.cuda.memory_cached()
+
+
+def _generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
+    def hook(self, *args):
+        if len(mem) == 0 or mem[-1]["exp"] != exp:
+            call_idx = 0
+        else:
+            call_idx = mem[-1]["call_idx"] + 1
+
+        mem_all, mem_cached = _get_gpu_mem()
+        torch.cuda.synchronize()
+        mem.append({
+            'layer_idx': idx,
+            'call_idx': call_idx,
+            'layer_type': type(self).__name__,
+            'exp': exp,
+            'hook_type': hook_type,
+            'mem_all': mem_all,
+            'mem_cached': mem_cached,
+        })
+
+    return hook

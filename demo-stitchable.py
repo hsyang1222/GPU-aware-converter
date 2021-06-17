9b4d431128bc3bc6a7279dd6174ecf84274561d7
torasan <grazinglion@gmail.com>
2021-06-17 16:55:05 +0900

    typo

--- demo-stitchable.py
+++ demo-stitchable.py
@@ -10,27 +10,35 @@ def test_stitch():
     bias = torch.rand(64)
 
     # ---------------------------- stitchable conv2d ---------------------------- #
+    print("start")
     input = input.clone().requires_grad_(True)
     model = StitchableConv2d(64,64,3,1,1, fetch_shape=[512,512], use_tqdm=True).cuda()
     model.weight.data = weight.clone().cuda()
     model.bias.data = bias.clone().cuda()
     out = model(input)
     out.sum().backward()
+    print("end")
 
 def test_non_stitch():
-    input = torch.rand(1,64,512,512)
+    input = torch.rand(1,64,4096,4096)
     weight = torch.rand(64,64,3,3)
     bias = torch.rand(64)
 
     # ---------------------------- vanila conv2d ---------------------------- #
+    print("start")
     input = input.clone().cuda().requires_grad_(True)
     model = nn.Conv2d(64,64,3,1,1).cuda()
     model.weight.data = weight.clone().cuda()
     model.bias.data = bias.clone().cuda()
     out = model(input)
     out.sum().backward()
+    print("end")
 
 if __name__ == "__main__":
+    #
+    # python demo-stitchable.py --use_stitch 1 # working
+    # python demo-stitchable.py --use_stitch 0 # OOM
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--use_stitch", type=int, default=0)
     args = parser.parse_args()
